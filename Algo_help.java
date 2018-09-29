9acec3e5ed375e2f948570ee79272297677512d4
Tharaka Ratnayake <tharakasachintha27@gmail.com>
2018-09-29 14:26:03 +0530

    Update Algo_help.java

--- Algo_help.java
+++ Algo_help.java
@@ -114,4 +114,95 @@ static int[] Merge(int[] A,int[] B){
     }
     return result;
 }
+ 
+ 
+ 
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//  
+//                  Greedy Method
+//   
+//            Used For optimization problems 
+//            Which mean for finding minimum or maximum
+//
+//           Methods used for optimizaiton probelms
+//
+//            1.Greedy Method
+//            2.Dynamic Programming
+//            3.Branch and Bround
+//
+//     
+//      a = [ feature space ]
+//      Algorithm Greedy(a,n){
+//       
+//       for i = 1 to n do{
+//          x = select(a)
+//         if feasible(x) then
+//                  solution = solution +x
+//        }
+//       }
+//
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+
+
+// This method can be used when you can choose a fractional vlaur
+// to fill up the knapsack. 
+// When you can't pick fractional values you have to solve it using 
+// Dynamic programming.
+static float KnapsackProblem_Greedy(int[][] list , int weight){
+
+float [][] ratio = new float[list.length][3];
+for(int i = 0; i < list.length; i++){
+    ratio[i][0] = list[i][0];  //profit
+    ratio[i][1] = list[i][1];  //weight
+    ratio[i][2] = (float)list[i][0] / list[i][1];
+}
+Arrays.sort(ratio, new Comparator<float[]>() {
+    public int compare(float[] s1, float[] s2) {
+        if (s1[2] < s2[2])
+            return 1;    // tells Arrays.sort() that s1 comes after s2
+        else if (s1[2]> s2[2])
+            return -1;   // tells Arrays.sort() that s1 comes before s2
+        else {
+            /*
+             * s1 and s2 are equal.  Arrays.sort() is stable,
+             * so these two rows will appear in their original order.
+             * You could take it a step further in this block by comparing
+             * s1[1] and s2[1] in the same manner, but it depends on how
+             * you want to sort in that situation.
+             */
+            return 0;
+        }
+    }
+});
+
+//for(int i = 0 ; i< ratio.length;i++){
+//   System.out.print(ratio[i][0]);
+//   System.out.print(" - ");
+//   System.out.print(ratio[i][1]);
+//   System.out.print(" - ");
+//   System.out.print(ratio[i][2]);
+//   System.out.println();
+//}
+float current_weight = 0;
+float profit = 0 ;
+for(int i = 0 ; i< list.length; i++){
+    
+    // If adding Item won't overflow, add it completely 
+    if(current_weight + ratio[i][1]<= weight){
+       current_weight += ratio[i][1];
+       profit += ratio[i][0];
+    }
+    
+    // If we can't add current Item, add fractional part of it 
+    else{
+        float remain = weight - current_weight;
+        profit +=  ratio[i][2]*remain;
+        break;
+    }
+}
+   return profit;
+}
+
 }
